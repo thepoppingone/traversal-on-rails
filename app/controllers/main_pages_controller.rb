bb956231f2f4c48c2a95b25dc0a921a26d91bf57
wangpp <wangpp1@hotmail.com>
2015-06-27 21:43:18 +0800

    Code further refactored and cleaned up, IMPORTANT: weather report is not by Country BUT by City

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -6,28 +6,28 @@ class MainPagesController < ApplicationController
   end
 
   def list_results
-  @country = params[:country]
+  @city = params[:city]
   @start_date_year = params[:start_date]
   
-  #@displayString = OpenWeather::Current.city(@country) Using openweather gem
+  #@displayString = OpenWeather::Current.city(@city) Using openweather gem
   
-      def find_by_country(country) #returns as a JSON
+      def find_by_city(city) #returns as a JSON
          #use HTTP to call random APIs using a query string
-          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric'})
+          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => city, :units => 'metric'})
       end
       
-      def find_by_country_forecast(country, days)
+      def find_by_city_forecast(city, days)
         
-          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric', :cnt => days})
+          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => city, :units => 'metric', :cnt => days})
       end
-#  puts find_by_country(@country).body #(shows up in the Rails server)
-  weather_info = JSON.parse(find_by_country(@country).body)
+#  puts find_by_city(@city).body #(shows up in the Rails server)
+  weather_info = JSON.parse(find_by_city(@city).body)
   @displayString = weather_info["main"]["temp"].to_s + "°C"
     @displayString2 = weather_info["main"]["temp_min"].to_s + "°C"
       @displayString3 = weather_info["main"]["temp_max"].to_s + "°C"
       
-      weather_forecast = JSON.parse(find_by_country_forecast(@country, 14).body)
-      #puts find_by_country_forecast(@country,14).body
+      weather_forecast = JSON.parse(find_by_city_forecast(@city, 14).body)
+      #puts find_by_city_forecast(@city,14).body
       #@displayForecast =  weather_forcast
       @displayForecast = weather_forecast["main"]["temp"].to_s + "°C"
        @displayForecast2 = weather_forecast["main"]["temp_min"].to_s + "°C"
