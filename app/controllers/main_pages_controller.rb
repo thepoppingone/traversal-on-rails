39cb21aea7f90651e91396cfd684eb1aebd9ed36
wangpp <wangpp1@hotmail.com>
2015-06-27 21:37:39 +0800

    2 weeks forecast added, display updated to reflect results information

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -15,11 +15,23 @@ class MainPagesController < ApplicationController
          #use HTTP to call random APIs using a query string
           HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric'})
       end
-  # puts find_by_country(@country).body (shows up in the Rails server)
-  weatherinfo = JSON.parse(find_by_country(@country).body)
-  @displayString = weatherinfo["main"]["temp"]
-    @displayString2 = weatherinfo["main"]["temp_min"]
-      @displayString3 = weatherinfo["main"]["temp_max"]
+      
+      def find_by_country_forecast(country, days)
+        
+          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric', :cnt => days})
+      end
+#  puts find_by_country(@country).body #(shows up in the Rails server)
+  weather_info = JSON.parse(find_by_country(@country).body)
+  @displayString = weather_info["main"]["temp"].to_s + "°C"
+    @displayString2 = weather_info["main"]["temp_min"].to_s + "°C"
+      @displayString3 = weather_info["main"]["temp_max"].to_s + "°C"
+      
+      weather_forecast = JSON.parse(find_by_country_forecast(@country, 14).body)
+      #puts find_by_country_forecast(@country,14).body
+      #@displayForecast =  weather_forcast
+      @displayForecast = weather_forecast["main"]["temp"].to_s + "°C"
+       @displayForecast2 = weather_forecast["main"]["temp_min"].to_s + "°C"
+        @displayForecast3 = weather_forecast["main"]["temp_max"].to_s + "°C"
 
   end
 end
