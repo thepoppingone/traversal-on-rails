ed5c6ee21a64f11310090fbfad6c26e9ebca7744
wangpp <wangpp1@hotmail.com>
2015-06-27 20:49:36 +0800

    Code refactoring and documentation

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -8,34 +8,18 @@ class MainPagesController < ApplicationController
   def list_results
   @country = params[:country]
   @start_date_year = params[:start_date]
-  #@client = YahooWeather::Client.new
-  #response = @client.lookup_by_woeid(2488892)
-  #puts "TEST: "+response.title
-  @displayString = OpenWeather::Current.city(@country)
-
-  #response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=Singapore&units=metric')
-  #puts response.body
-  #@displayString = JSON.parse(response.body)
-
-  #displayString = OpenWeather::Current.city(@country)
   
-  #testing
-  #@displayString = JSON.parse(response.body)
+  #@displayString = OpenWeather::Current.city(@country) Using openweather gem
   
-  #@testWeather = response.page_url
-  #write application logic here
-
-  def find_by_country(country)
-   HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric'})
-  end
-  puts find_by_country(@country).body
-  #@displayString = find_by_country(@country).body
+      def find_by_country(country) #returns as a JSON
+         #use HTTP to call random APIs using a query string
+          HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :query => {:q => country, :units => 'metric'})
+      end
+  # puts find_by_country(@country).body (shows up in the Rails server)
   weatherinfo = JSON.parse(find_by_country(@country).body)
   @displayString = weatherinfo["main"]["temp"]
     @displayString2 = weatherinfo["main"]["temp_min"]
       @displayString3 = weatherinfo["main"]["temp_max"]
-
-
 
   end
 end
