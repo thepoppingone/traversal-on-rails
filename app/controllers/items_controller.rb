fc345527d5e96995c6a7b6575c24f3bbae915626
wangpp <wangpp1@hotmail.com>
2015-07-27 19:00:26 +0800

    permitted parameters for quantity

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,6 +61,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :season, :itemType, :minTemp, :maxTemp, :list_id)
+      params.require(:item).permit(:name, :season, :quantity , :list_id)
     end
 end
