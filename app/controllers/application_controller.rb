7ac6c724eecd9e1b5a2b563d8dc06e5d6bf2145b
ronald <ronald_kan@hotmail.com>
2015-07-31 21:33:09 +0800

    profile avatar done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protect_from_forgery with: :exception
-  
+
   def authenticate_admin_user!
     redirect_to new_user_session_path unless current_user.admin?
   end
- 
+
     protected
-    
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :bio]
-      devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :bio]
+      devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :bio, :avatar]
     end
- 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726021519) do
+ActiveRecord::Schema.define(version: 20150731132156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,25 @@ ActiveRecord::Schema.define(version: 20150726021519) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "fb_users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "image"
+    t.string   "token"
+    t.datetime "expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fbs", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "season"
@@ -59,6 +78,14 @@ ActiveRecord::Schema.define(version: 20150726021519) do
 
   add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
 
+  create_table "user2s", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -78,6 +105,13 @@ ActiveRecord::Schema.define(version: 20150726021519) do
     t.string   "last_name"
     t.string   "bio"
     t.boolean  "admin"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
