b3eaa4b50fdc7eb33f3d7ea467e354278568088a
wangpp <wangpp1@hotmail.com>
2015-08-11 15:50:53 +0800

    test on facebook login password default

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ def self.new_with_session(params, session)
   super.tap do |user|
      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
        user.email = data["email"] if user.email.blank?
+       user.password = "password"
      end
    end
  end
